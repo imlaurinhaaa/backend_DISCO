eed379f32ab7fc791c01293f741a3499b58cec79
AlbaGomesG <162628812+AlbaGomesG@users.noreply.github.com>
2025-11-27 09:23:57 -0300

    adding getGenres

--- src/routes/singerRoutes.js
+++ src/routes/singerRoutes.js
@@ -4,6 +4,7 @@ const singerController = require("../controllers/singerController");
 const upload = require("./../config/upload.js");
 
 router.get("/singers", singerController.getSingers);
+router.get("/genres", singerController.getGenresController);
 router.get("/singers/:id", singerController.getSingerById);
 router.post("/singers", upload.single("photo"), singerController.createSinger);
 router.put("/singers/:id", upload.single("photo"), singerController.updateSinger);
